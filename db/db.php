c33436e5a2506105e3b178302d280805ab23f502
Ihor Butenko <91276970+IhorDeveloper@users.noreply.github.com>
2022-11-10 21:11:49 +0200

    Add database connection

---
+++ db/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$db['host'] = 'localhost';
+$db['username'] = 'admin';
+$db['password'] = '1234';
+$db['name'] = "cms";
+
+foreach($db as $key => $value){
+
+    define($key, $value);
+
+}
+
+$connect = new mysqli(host, username, password, name);
+
+?>
